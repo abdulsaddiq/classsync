789170d31e721878a6762d473cfb06e4a7a67513
SADDIQ <shaikabdulsadiqumoorbasha@outlook.com>
2026-04-07 22:08:02 +0530

    Connected frontend to backend

--- frontend/script.js
+++ frontend/script.js
@@ -1,4 +1,4 @@
-const API = 'http://127.0.0.1:5000/api';
+const API = 'https://classsync-7nxx.onrender.com';
 
 let SUBJECTS = ['DS', 'DEVC', 'Maths', 'Physics', 'Chemistry', 'English', 'EVS'];
 let posts = [];
