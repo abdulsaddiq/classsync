8765bff5c4924b2bb303f4d53e70483f784c0c4b
SADDIQ <shaikabdulsadiqumoorbasha@outlook.com>
2026-04-07 22:01:26 +0530

    Added requirements.txt

--- app.py
+++ app.py
@@ -19,4 +19,6 @@ def index():
     return {'message': 'ClassSync API running'}
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
